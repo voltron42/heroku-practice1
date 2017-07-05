60142a1467300d685724bebd23ce887a4e626769
Daniel Johnson <dajohnson1978@gmail.com>
2017-07-05 06:32:47 -0500

    adjusting dependencies, removing server controls

--- bin/heroku_practice1/core.clj
+++ bin/heroku_practice1/core.clj
@@ -1,22 +1,16 @@
 (ns heroku-practice1.core
-  (:require [org.httpkit.server :refer:all]))
+  (:require [org.httpkit.server :as http]
+            [clojure.xml :as x]
+            [environ.core :refer [env]]))
 
-(defonce server (atom nil))
-
-(defn stop-server []
-  (when-not (nil? @server)
-    ;; graceful shutdown: wait 100ms for existing requests to be finished
-    ;; :timeout is optional, when no timeout, stop immediately
-    (@server :timeout 100)
-    (reset! server nil)))
-
-(defn -main [& args]
-  (let [app #(-> {:status 200 
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))
+        app #(-> {:status 200 
                   :headers "text/html"
-                  :body (emit-element {:tag :html
-                                       :content [{:tag :body
-                                                  :content [{:tag :h2
-                                                             :content ["Hello World!"]}
-                                                            {:tag :p
-                                                             :content ["This is a practice app!"]}]}]})})]
-    (reset! server (run-server #'app {:port 5000}))))
+                  :body (x/emit-element {:tag :html
+                                         :content [{:tag :body
+                                                    :content [{:tag :h2
+                                                               :content ["Hello World!"]}
+                                                              {:tag :p
+                                                               :content ["This is a practice app!"]}]}]})})]
+    (http/run-server #'app {:port port :join? false})))
--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [http-kit "2.2.0"]]
-    :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [http-kit "2.2.0"]
+                 [environ "1.0.0"]]
+  :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "heroku-practice1.jar"
